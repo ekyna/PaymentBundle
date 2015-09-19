4677f208d697b8a7279fa5aa4132870f8341bc24
Etienne Dauvergne <contact@ekyna.com>
2015-09-19 10:18:43 +0200

    [PaymentBundle] Admin form : method as select, amount as money.

--- Form/Type/PaymentType.php
+++ Form/Type/PaymentType.php
@@ -2,8 +2,12 @@
 
 namespace Ekyna\Bundle\PaymentBundle\Form\Type;
 
+use Doctrine\ORM\EntityRepository;
+use Ekyna\Component\Sale\Payment\PaymentStates;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 /**
  * Class PaymentType
@@ -13,19 +17,64 @@ use Symfony\Component\Form\FormBuilderInterface;
 class PaymentType extends AbstractType
 {
     /**
+     * @var string
+     */
+    private $methodClass;
+
+
+    /**
+     * Constructor.
+     * @param string $methodClass
+     */
+    public function __construct($methodClass)
+    {
+        $this->methodClass = $methodClass;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('amount', 'number', array( // TODO static number type
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) use ($options) {
+            $form = $event->getForm();
+
+            /** @var \Ekyna\Bundle\PaymentBundle\Entity\Payment $payment */
+            $payment = $event->getData();
+
+            $disabled = (null !== $payment && $payment->getState() !== PaymentStates::STATE_NEW);
+            $currency = null !== $payment ? $payment->getCurrency() : 'EUR';
+
+            $form->add('amount', 'money', array(
                 'label' => 'ekyna_payment.payment.field.amount',
-                'disabled' => !$options['admin_mode'],
-            ))
-            ->add('method', 'ekyna_payment_method_choice', array(
-                'label' => 'ekyna_payment.payment.field.method',
-            ))
-        ;
+                'currency' => $currency,
+                'disabled' => $disabled || !$options['admin_mode'],
+            ));
+
+            if ($options['admin_mode']) {
+                $form->add('method', 'entity', array(
+                    'label'    => 'ekyna_payment.payment.field.method',
+                    'class'    => $this->methodClass,
+                    'property' => 'paymentName',
+                    'disabled' => $disabled,
+                    'query_builder' => function (EntityRepository $repository) {
+                        return $repository
+                            ->createQueryBuilder('m')
+                            ->andWhere('m.enabled = :enabled')
+                            ->andWhere('m.factoryName = :factoryName')
+                            ->setParameters(array(
+                                'enabled' => true,
+                                'factoryName' => 'offline',
+                            ))
+                        ;
+                    }
+                ));
+            } else {
+                $form->add('method', 'ekyna_payment_method_choice', array(
+                    'label'    => 'ekyna_payment.payment.field.method',
+                ));
+            }
+        });
     }
 
     /**
